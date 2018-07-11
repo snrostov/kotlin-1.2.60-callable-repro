cb34c35f053f7e16d415c5034fd34caee6ee06a7
Sergey Rostov <Sergey.Rostov@jetbrains.com>
2018-07-11 14:19:16 +0300

    Initial commit

---
+++ src/main/kotlin/test.kt
@@ -0,0 +1,14 @@
+package sample
+
+import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.jvm.javaMethod
+
+interface Sample {
+
+}
+
+fun main(args: Array<String>) {
+    Sample::class.memberFunctions.forEach {
+        it.javaMethod
+    }
+}
